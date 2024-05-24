cbe03f9002fda209c4beb02b7e9fbe0999eff02b
Faizanriaz44553 <sylanipractice@gmail.com>
2024-05-24 04:16:57 -0700

    project initialization

--- app.js
+++ app.js
@@ -1,45 +1,54 @@
 
-const result = document.querySelector("#result")
+function generatePassword() {
+  let para = document.querySelector("#result")
 // Sabhi lowercase alphabets
-const lowercaseAlphabets = [
+let lowercaseAlphabets = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
   ];
   
   // Sabhi uppercase alphabets
-  const uppercaseAlphabets = [
+  let uppercaseAlphabets = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
   ];
   
   // Numbers 1 se 9 tak
-  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   
   // Special characters
-  const specialCharacters = [
-    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '[',
-    ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?', '/', '~',
+  let specialCharacters = [
+    '!', '@', '#', '$', '%', '^', '&', '*', '_', '+', ';', ':', ',', '.', '<', '>', '?', '/', '~',
     '`', '-', '='
   ];
   
   // Combined array
-  const allCharacters = [
+  let allCharacters = [
     ...lowercaseAlphabets,
     ...uppercaseAlphabets,
     ...numbers,
     ...specialCharacters
   ];
-  
-let passwordvalidation = 10;
 
-function generatePassword(length) {
     let password = ''
-for (let i = 0; i < length; i++) {
+for (let i = 0; i < 10; i++) {
 let combineRemdon = Math.floor(Math.random() * allCharacters.length)
 password += allCharacters[combineRemdon]
 }
-return password;
+para.innerHTML = password
 }
-let password = generatePassword(passwordvalidation)
-console.log(password);
-  
+function copypaste() {
+  let passwordInput = document.querySelector("#result")
+  let alertMassage = document.querySelector("#alert-massage")
+  navigator.clipboard.writeText(passwordInput.value);
+
+  if (passwordInput.value) {
+    Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "Copy Password",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
+}
